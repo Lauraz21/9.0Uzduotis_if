a666cdae42d9593a76ef1a8c7fa824f1928a8a0b
Laura <Laura@DESKTOP-BESSB6E>
2023-08-25 18:38:04 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿while (true)
+{
+    Console.WriteLine("Iveskite menesi: ");
+    string month = Console.ReadLine();
+    if (month == "sausis" || month == "vasaris" || month == "kovas")
+    {
+        Console.WriteLine("Saltas laikotarpis");
+    }
+    if (month == "birzelis" || month == "liepa" || month == "rugpjutis")
+    {
+        Console.WriteLine("Karstas laikotarpis");
+    }
+    if (month == "balandis" || month == "geguze" ||
+        month == "rugsejis" || month == "spalis" ||
+        month == "lapkritis" || month == "gruodis")
+
+    {
+        Console.WriteLine("Vidutinio laikotarpio menuo");
+    }
+    Console.ReadLine();
+    Console.Clear();
+}
